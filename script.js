703d6d2d3205c7c2db3b7337afbf46f991f053e3
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2024-03-04 21:46:12 -0300

    Style Form

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ window.addEventListener("focus", () => {
     document.getElementById("portfolio").classList.add("visible")
   }, 1000)
 })
+
+window.addEventListener("blur", () => {
+  document.title = "Come back"
+  document.getElementById("send_mensage").classList.remove("visible")
+  document.getElementById("send_mensage").classList.add("hidden")
+})
+window.addEventListener("focus", () => {
+  document.title = docTitle
+  setTimeout(function () {
+    document.getElementById("send_mensage").classList.remove("hidden")
+    document.getElementById("send_mensage").classList.add("visible")
+  }, 1000)
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  var seta = document.getElementById("seta")
+
+  seta.addEventListener("click", function () {
+    var newX = 30
+    // var newY = Math.random() * (window.innerHeight - seta.offsetHeight)
+
+    if (send_mensage.style.left != newX + "px") {
+      send_mensage.style.left = newX + "px"
+      seta.classList.add("open")
+    } else {
+      send_mensage.style.left = "-220px"
+      seta.classList.remove("open")
+    }
+    // seta.style.top = newY + "px"
+  })
+})
