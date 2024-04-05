25f459dd59d096d75167e62f7f3b42954fbde7be
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2024-04-05 20:58:02 -0300

    Refactoring

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-let docTitle = document.title
-window.addEventListener("blur", () => {
-  document.title = "Come back :("
-  document.getElementById("portfolio").classList.remove("visible")
-  document.getElementById("portfolio").classList.add("hidden")
-})
-window.addEventListener("focus", () => {
-  document.title = docTitle
-  setTimeout(function () {
-    document.getElementById("portfolio").classList.remove("hidden")
-    document.getElementById("portfolio").classList.add("visible")
-  }, 1000)
-})
+document.addEventListener("DOMContentLoaded", () => {
+    "use strict"
 
-window.addEventListener("blur", () => {
-  document.title = "Come back :("
-  document.getElementById("send_mensage").classList.remove("visible")
-  document.getElementById("send_mensage").classList.add("hidden")
-})
-window.addEventListener("focus", () => {
-  document.title = docTitle
-  setTimeout(function () {
-    document.getElementById("send_mensage").classList.remove("hidden")
-    document.getElementById("send_mensage").classList.add("visible")
-  }, 1000)
-})
-
-document.addEventListener("DOMContentLoaded", function () {
-  var seta = document.getElementById("seta")
-
-  seta.addEventListener("click", function () {
-    var newX = 30
-    // var newY = Math.random() * (window.innerHeight - seta.offsetHeight)
-
-    if (send_mensage.style.left != newX + "px") {
-      send_mensage.style.left = newX + "px"
-      seta.classList.add("open")
-    } else {
-      send_mensage.style.left = "-220px"
-      seta.classList.remove("open")
+    const preloader = document.querySelector("#preloader")
+    if (preloader) {
+        console.log("hello")
+        setTimeout(() => {
+            preloader.classList.add("loaded")
+        }, 5000)
+        setTimeout(() => {
+            preloader.remove()
+        }, 2000000)
     }
-    // seta.style.top = newY + "px"
-  })
+
+    document.addEventListener("click", function (event) {
+        preloader.classList.add("loaded")
+        setTimeout(() => {
+            preloader.remove()
+        }, 2000)
+    })
 })
